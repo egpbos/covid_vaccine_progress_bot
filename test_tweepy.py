dd620930fa6addb8e53a7c91e953257bd4d29168
E. G. Patrick Bos <egpbos@gmail.com>
2021-02-11 10:28:12 +0100

    remove stringio use and remove literal keys :D

--- test_tweepy.py
+++ test_tweepy.py
@@ -1,16 +1,24 @@
 import io
 from collections import defaultdict
+import configargparse as cap
 
 import tweepy
 from tqdm import tqdm
 import pandas as pd
 import pycountry_convert as pc
 
+argparser = cap.ArgParser(default_config_files=['keys.yml'])
+argparser.add('-c', is_config_file=True, help='config file path')
+argparser.add('--api', env_var='BOT_API')
+argparser.add('--api-secret', env_var='BOT_API_SECRET')
+argparser.add('--access', env_var='BOT_ACCESS')
+argparser.add('--access-secret', env_var='BOT_ACCESS_SECRET')
+
+args = argparser.parse_args()
+
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("7YWpupaRoYdoLgRbF4ZVg3pLu", 
-    "13DB7FUUlESM1xhyHg2eGIA8v7W5VMip5zNp5WnTqoqbROTHrR")
-auth.set_access_token("1359506310044258304-29AM2zvbPXfSBfux9db2DPGPlZEU2f", 
-    "xdcfvElp9Dmg5J6xU4hSlZI9KMvI1cOPzQFMyT42eAcC3")
+auth = tweepy.OAuthHandler(args.api, args.api_secret)
+auth.set_access_token(args.access, args.access_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
@@ -19,8 +27,6 @@ try:
     print("Authentication OK")
 except:
     print("Error during authentication")
-
-bar_stream = io.StringIO()
 
 # get current percentage
 data = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv', parse_dates=['date'])
@@ -31,20 +37,12 @@ data_filtered = data_filtered[data_filtered.date == data_filtered.date.max()]
 # take last item in case dataset contains multiple items this day:
 percentage = data_filtered.iloc[-1].people_vaccinated_per_hundred
 
-bar = tqdm(initial=percentage, total=100., bar_format='|{bar:15}| {percentage:3.1f}%', file=bar_stream, ascii=False)
+bar = tqdm(initial=percentage, total=100., bar_format='|{bar:15}| {percentage:3.1f}%', ascii=False)
 
-def pop_stream_value(stream):
-    value = stream.getvalue()
-    stream.truncate(0)
-    stream.seek(0)
-    return value
-
-
-bar_string = pop_stream_value(bar_stream)
+bar_string = bar.__str__()
 print("bar_string:\n", bar_string)
-del bar
-tweet_string = bar_string[:-5].replace(' ', '\u3000') + bar_string[-5:] + '\n'
-# tweet_string = tweet_string.replace('\r', '')
+bar.close()
+tweet_string = bar_string[:-5].replace(' ', '\u3000') + bar_string[-5:]
 
 # add continent bars
 
@@ -84,26 +82,19 @@ strings = {}
 for continent, total in continent_totals.items():
     continent_percentages[continent] = total / total_pop_continents[continent] * 100
 
-    bar_stream = io.StringIO()
     bar = tqdm(initial=continent_percentages[continent],
                total=100., bar_format='|{bar:12}| C: {percentage:3.1f}%',
-               file=bar_stream, ascii=False)
-    bar_string = pop_stream_value(bar_stream)
-    bar_stream.close()
-    del bar
-    tweet_string_add = "\n\n" + bar_string[:-8].replace(' ', '\u3000') + bar_string[-8:] 
-    # tweet_string_add = tweet_string_add.replace('\r', '')
-    # tweet_string_add = tweet_string_add.replace('\n', '')
+               ascii=False)
+    bar_string = bar.__str__()
+    bar.close()
+    tweet_string_add = bar_string[:-8].replace(' ', '\u3000') + bar_string[-8:] 
     tweet_string_add = tweet_string_add.replace('C', continent)
-    print("add:", tweet_string_add)
     strings[continent] = tweet_string_add
-    tweet_string = tweet_string + tweet_string_add
+    tweet_string = tweet_string + "\n" + tweet_string_add
 
-
-# tweet_string += "Data: https://ourworldindata.org/covid-vaccinations"
 
 print("final string:")
 print(tweet_string)
 
 # and update on twitter
-# api.update_status(tweet_string)
+api.update_status(tweet_string)
